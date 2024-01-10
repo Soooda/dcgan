daf1796240b5d4b12a758aa74b044013e1a5c2e8
Soooda <kongliangxm@foxmail.com>
2024-01-10 20:22:50 +0900

    Update model architecture

--- model/dcgan.py
+++ model/dcgan.py
@@ -1,51 +1,72 @@
 import torch
 import torch.nn as nn
 
-class DCDiscriminator(nn.Module):
-    def __init__(self, input_dimension, channel):
-        super(DCDiscriminator, self).__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(channel, input_dimension, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(input_dimension, input_dimension * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(input_dimension * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(input_dimension * 2, input_dimension * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(input_dimension * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(input_dimension * 4, input_dimension * 8, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(input_dimension * 8, 1, 4, 2, 1, bias=False),
-            nn.Sigmoid(),
+
+'''
+From the DCGAN paper, the authors specify that all model weights shall be randomly initialized from a Normal distribution with mean=0, stdev=0.02. The weights_init function takes an initialized model as input and reinitializes all convolutional, convolutional-transpose, and batch normalization layers to meet this criteria. This function is applied to the models immediately after initialization.
+'''
+# Custom weights initialization
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+
+class Generator(nn.Module):
+    def __init__(self, ngpu, nz, nc):
+        ngf = 64
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
         )
 
-    def forward(self, data):
-        return self.model(data)
+    def forward(self, input):
+        return self.main(input)
     
-class DCGenerator(nn.Module):
-    def __init__(self, input_dimension, channel, latent_size=100):
-        super(DCGenerator, self).__init__()
-        self.input_dimension = input_dimension
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(latent_size, input_dimension * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(input_dimension * 8),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(input_dimension * 8, input_dimension * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(input_dimension * 4),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(input_dimension * 4, input_dimension * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(input_dimension * 2),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(input_dimension * 2, input_dimension, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(input_dimension * 2),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(input_dimension, channel, 4, 2, 1, bias=False),
-            nn.Tanh(),
+class Discriminator(nn.Module):
+    def __init__(self, ngpu, nc):
+        ndf = 64
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
         )
 
-    def forward(self, data):
-        return self.model(data)
+    def forward(self, input):
+        return self.main(input)
+    
 
-    def generate_noise(self, n):
-        noise = torch.randn(n, self.input_dimension ** 2)
-        return noise
+if __name__ == "__main__":
+    G = Generator(1, 100, 28, 1)
+    print(G)
+    D = Discriminator(1, 1, 28)
+    print(D)
